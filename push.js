95066deb72b211da1439a204c40d8c9df8687a47
angelozerr <angelo.zerr@gmail.com>
2015-11-10 22:05:48 +0100

    Provide sendToClient for the tern server.

--- push.js
+++ push.js
@@ -1,23 +1,27 @@
 (function(mod) {
     if (typeof exports == "object" && typeof module == "object") // CommonJS
-        return mod(require("tern/lib/infer"), require("tern/lib/tern"));
+        return mod(require("tern/lib/tern"));
     if (typeof define == "function" && define.amd) // AMD
-        return define([ "tern/lib/infer", "tern/lib/tern" ], mod);
-    mod(tern, tern);
-})(function(infer, tern) {
+        return define(["tern/lib/tern" ], mod);
+    mod(tern);
+})(function(tern) {
     "use strict";
-    
+  
+  function createWebSocketServer(server) {
+    var WebSocketServer = require('ws').Server;
+    server.wss = new WebSocketServer({ server: server.parent });
+  }
+  
+  tern.Server.prototype.sendToClient = function(data) {
+    var wss = this.wss;
+    if (wss) wss.clients.forEach(function each(client) { client.send(JSON.stringify(data)); }); // node context
+    else this.signal('sendToClient', data); // browser context
+  };
+  
   tern.registerPlugin("push", function(server, options) {
-    if (server.httpServer) {
-      var WebSocketServer = require('ws').Server, wss = new WebSocketServer({ server: server.httpServer })
-      wss.on('connection', function connection(ws) {
-        ws.on('message', function incoming(message) {
-          console.log('received: %s', message);
-        });
-
-        ws.send('something');
-      });
-    }
+    if (server.parent) {
+      createWebSocketServer(server);
+    }  
   });
     
 })
